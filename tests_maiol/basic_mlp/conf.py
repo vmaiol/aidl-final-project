40c5257a1c2f8ef030a33de93d4357fa3ce3131d
vmaiol <75121950+vmaiol@users.noreply.github.com>
2023-03-16 12:04:35 +0100

    Add files via upload

--- tests_maiol/basic_mlp/conf.py
+++ tests_maiol/basic_mlp/conf.py
@@ -1,7 +1,6 @@
 #constantes conf vars
 conf_vars = {
-    #ray tune
-    "MAX_NUM_EPOCHS" : 10, # es un máximo. Si en la connfig ponemos que rayTune escoja entre los valores de la lista, esta variable será el limite
+    "MAX_NUM_EPOCHS" : 10,
     "NUM_SAMPLES" : 1, #numero de veces a ejecutar la config/search
     "DATA_DIR" : "./../data_sample_victor",
     #"DATA_DIR" : "/home/juanrivas/data_reduced/"
