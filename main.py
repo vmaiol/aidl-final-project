a4963ea42eec862b2ab2804d2561e8e538d8794e
Rivas22 <juan.rivas@estudiant.upc.edu>
2023-03-17 14:36:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+import netCDF4 as nc
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, random_split
+from torchvision.datasets import ImageFolder
+import os
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as T
+import numpy as np
+
+
+from model import *
+from dataset import *
+from resources import *
+
+path = "data_sample"
+
+torch.manual_seed(0)
+
+hparams = {
+    'batch_size':10,
+    'num_epochs':20,
+    'test_batch_size':10,
+    'hidden_size':128,
+    'num_classes':1,
+    'learning_rate':1e-4,
+    'log_interval':2,
+    'early:stop': True
+}
+
+hparams['device'] = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+files = os.listdir(path)
+dataset = Downscaling_dataset(path)
+
+train, test, val = torch.utils.data.random_split(dataset, [0.7,0.2,0.1])
+
+data_loader = test_loader = torch.utils.data.DataLoader(dataset, batch_size=hparams['batch_size'], drop_last=True, shuffle=True)
+train_loader = test_loader = torch.utils.data.DataLoader(train, batch_size=hparams['batch_size'], drop_last=True, shuffle=True)
+test_loader = test_loader = torch.utils.data.DataLoader(test, batch_size=hparams['batch_size'], drop_last=True, shuffle=True)
+val_loader = test_loader = torch.utils.data.DataLoader(val, batch_size=hparams['batch_size'], drop_last=True, shuffle=True)
+
+model = Downscaling_model1().to(hparams['device'])
+optimizer = torch.optim.Adam(model.parameters(), hparams['learning_rate'])
+criterion = nn.MSELoss()
+
+train_losses = []
+test_losses = []
+test_accs = []
+
+# For each epoch
+for epoch in range(1, hparams['num_epochs'] + 1):
+
+  # Compute & save the average training loss for the current epoch
+  train_loss = train_epoch(train_loader, model, optimizer, criterion, hparams, epoch)
+  train_losses.append(train_loss)
+
+  # TODO: Compute & save the average test loss & accuracy for the current epoch
+  # HELP: Review the functions previously defined to implement the train/test epochs
+  test_loss, test_accuracy = test_epoch(test_loader, model, criterion, hparams)
+
+  test_losses.append(test_loss)
+  test_accs.append(test_accuracy)
+  break
---
+++ model.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+class Downscaling_model1(nn.Module):
+  def __init__(self):
+    super(Downscaling_model1, self).__init__()
+    self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 16 , kernel_size = 3, stride = 2 , padding = 1) # out --> 
+    self.conv2 = nn.Conv2d(in_channels = 16, out_channels = 32, kernel_size= 3, stride = 2, padding= 1) # out
+    self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size= 3, stride = 2, padding= 1) # out
+    self.conv4 = nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size= 3, stride = 2, padding= 1) # out
+    self.fc = nn.Linear(in_features=128,out_features = 1)
+    self.maxpool = nn.MaxPool2d(kernel_size=2)
+    self.dropout = nn.Dropout(0)
+
+  def forward(self, x: torch.Tensor):
+    out = self.maxpool(F.relu(self.dropout(self.conv1(x))))
+    out = self.maxpool(F.relu(self.dropout(self.conv2(out))))
+    out = self.maxpool(F.relu(self.dropout(self.conv3(out))))
+    out = self.maxpool(F.relu(self.dropout(self.conv4(out))))
+    bsz,chn,w,h = out.shape
+    out = out.view(bsz,-1)
+    out = self.fc(out)
+
+    return out
