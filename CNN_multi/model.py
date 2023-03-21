e9ee494308e251d3cef4ef5307a1e9d72487a993
Rivas22 <juan.rivas@estudiant.upc.edu>
2023-03-21 22:03:43 +0100

    model multivariable update

--- CNN_multi/model.py
+++ CNN_multi/model.py
@@ -29,7 +29,8 @@ class Downscaling_model2(nn.Module):
     self.conv1 = nn.Conv2d(in_channels = 8, out_channels = 16 , kernel_size = 2, stride = 2 , padding = 1) # out --> 
     self.conv2 = nn.Conv2d(in_channels = 16, out_channels = 32, kernel_size= 2, stride = 2, padding= 1) # out
     self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size= 2, stride = 2, padding= 1) # out
-    self.fc = nn.Linear(in_features=576,out_features = 1)
+    self.conv4 = nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size= 2, stride = 2, padding= 1) # out
+    self.fc = nn.Linear(in_features=128,out_features = 1)
     self.maxpool = nn.MaxPool2d(kernel_size=2)
     self.dropout = nn.Dropout(0)
 
@@ -37,6 +38,7 @@ class Downscaling_model2(nn.Module):
     out = self.maxpool(F.relu(self.dropout(self.conv1(x))))
     out = self.maxpool(F.relu(self.dropout(self.conv2(out))))
     out = self.maxpool(F.relu(self.dropout(self.conv3(out))))
+    out = self.maxpool(F.relu(self.dropout(self.conv4(out))))
     bsz,chn,w,h = out.shape
     out = out.view(bsz,-1)
     out = self.fc(out)
